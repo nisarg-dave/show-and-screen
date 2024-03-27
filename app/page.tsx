4180a8962c71cab96e7b0414ee43d00b09efa213
Nisarg Dave <ndave630@gmail.com>
2024-03-27 21:08:53 +0800

    Created a carousel for trending tv-shows

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,33 @@
 import MovieCarousel from "@/components/MovieCarousel";
 import Image from "next/image";
 
-async function getData() {
+async function getTrendingMovies() {
   const res = await fetch("https://api.themoviedb.org/3/trending/movie/week", {
     headers: {
       Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
     },
   });
+  return res.json();
+}
 
+async function getTrendingTvShows() {
+  const res = await fetch("https://api.themoviedb.org/3/trending/tv/week", {
+    headers: {
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  });
   return res.json();
 }
 
 export default async function Home() {
-  const data = await getData();
+  const trendingMovies = await getTrendingMovies();
+  const trendingTvShows = await getTrendingTvShows();
 
   return (
     <main className="min-h-screen min-w-screen">
       <div className="my-4 mx-4">
-        <MovieCarousel movies={data.results} />
+        <MovieCarousel movies={trendingMovies.results} />
+        <MovieCarousel movies={trendingTvShows.results} />
       </div>
     </main>
   );
