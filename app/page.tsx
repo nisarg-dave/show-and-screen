12863e01f164168edcd8e322d9911789ab4c4ba5
Nisarg Dave <ndave630@gmail.com>
2024-04-21 12:06:18 +0800

    Added more lists from tmdb api

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,42 @@ async function getTrendingTvShows() {
   return res.json();
 }
 
+async function getNowPlayingMovies() {
+  const res = await fetch("https://api.themoviedb.org/3/movie/now_playing", {
+    next: { revalidate: 3600 },
+    headers: {
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  });
+  return res.json();
+}
+
+async function getUpcomingMovies() {
+  const res = await fetch("https://api.themoviedb.org/3/movie/upcoming", {
+    next: { revalidate: 3600 },
+    headers: {
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  });
+  return res.json();
+}
+
+async function getTopRatedTvShows() {
+  const res = await fetch("https://api.themoviedb.org/3/tv/top_rated", {
+    next: { revalidate: 3600 },
+    headers: {
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  });
+  return res.json();
+}
+
 export default async function Home() {
   const trendingMovies = await getTrendingMovies();
   const trendingTvShows = await getTrendingTvShows();
+  const nowPlayingMovies = await getNowPlayingMovies();
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedTvShows = await getTopRatedTvShows();
   const client = getClient();
   const { data } = await client.query({
     query: UserQueryDocument,
@@ -71,6 +104,18 @@ export default async function Home() {
           Trending TV Shows
         </h2>
         <MediaCarousel tmdbMedia={trendingTvShows.results} isTmdb={true} />
+        <h2 className="text-secondary font-semibold text-xl">
+          Now Playing Movies
+        </h2>
+        <MediaCarousel tmdbMedia={nowPlayingMovies.results} isTmdb={true} />
+        <h2 className="text-secondary font-semibold text-xl">
+          Upcoming Movies
+        </h2>
+        <MediaCarousel tmdbMedia={upcomingMovies.results} isTmdb={true} />
+        <h2 className="text-secondary font-semibold text-xl">
+          Top Rated TV Shows
+        </h2>
+        <MediaCarousel tmdbMedia={topRatedTvShows.results} isTmdb={true} />
       </div>
     </main>
   );
