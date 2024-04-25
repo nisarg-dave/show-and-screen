5b43c293a6e0b87024e46b9aac19aabbb2e3727d
Nisarg Dave <ndave630@gmail.com>
2024-04-25 12:31:10 +0800

    Created file for tmdb types

---
+++ types/Tmdb.ts
@@ -0,0 +1,35 @@
+export type TmdbTvShow = {
+  backdrop_path: string;
+  id: number;
+  original_name: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  adult: boolean;
+  name: string;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  first_air_date: string;
+  vote_average: number;
+  vote_count: number;
+  origin_country: string[];
+};
+
+export type TmdbMovie = {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  title: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
