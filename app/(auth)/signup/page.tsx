7c0dac7270b9c9e6add544f4d26110a4172f3e2b
Nisarg Dave <ndave630@gmail.com>
2024-08-24 15:49:13 +0800

    Completed UI of singup form

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,12 +1,16 @@
 import SignUpForm from "@/components/authentication/SignUpForm";
+import Image from "next/image";
 import React from "react";
 
 function page() {
   return (
     <div className="text-secondary">
-      <h1 className="text-2xl font-semibold tracking-tight mb-2">
-        Create an account
-      </h1>
+      <div className="flex justify-center mb-2 gap-2">
+        <Image src="/reel.png" alt="Reel" width={50} height={50} />
+        <h1 className="text-3xl font-bold tracking-tight text-center pt-2">
+          Show and Screen
+        </h1>
+      </div>
       <SignUpForm />
     </div>
   );
--- components/authentication/SignUpForm.tsx
+++ components/authentication/SignUpForm.tsx
@@ -13,6 +13,7 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -33,46 +34,76 @@ function SignUpForm() {
     console.log(values);
   };
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input
-                  className="bg-muted-foreground"
-                  placeholder="example@example.com"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input
-                  className="bg-muted-foreground"
-                  type="password"
-                  placeholder="••••••••"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="border-border border-2 p-4 bg-primary w-[35rem] h-[30rem] rounded-md">
+      <h1 className="text-2xl font-semibold tracking-tight mb-2 text-center">
+        Create an account
+      </h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-muted-foreground"
+                    placeholder="example@example.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-muted-foreground"
+                    type="password"
+                    placeholder="••••••••"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="w-full bg-card-foreground hover:bg-purple-800"
+          >
+            Submit
+          </Button>
+        </form>
+      </Form>
+      <div className="relative mt-6">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t"></span>
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">Or</span>
+        </div>
+      </div>
+      <Button
+        type="submit"
+        className="w-full bg-card-foreground hover:bg-purple-800 mt-6"
+      >
+        <Image src="/Google.svg" alt="Google" width={40} height={40} />
+        <p className="ml-2">Sign up with Google</p>
+      </Button>
+      <div className="flex justify-end mt-9">
+        <span className=" cursor-pointer hover:underline text-sm">
+          Already have an account?
+        </span>
+      </div>
+    </div>
   );
 }
 
