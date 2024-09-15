d0c87aa242762992c18cbb57437d0f67ba067c63
Nisarg Dave <ndave630@gmail.com>
2024-09-15 16:33:28 +0800

    Added instructions

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -69,43 +69,95 @@ export default async function Home() {
         <h2 className="text-secondary font-semibold text-xl">
           My Top 5 Movies
         </h2>
-        <MediaCarousel
-          movies={currentUser?.topFiveMovies}
-          isMovie={true}
-          isTopFiveMovie={true}
-        />
+        {currentUser.topFiveMovies.length > 0 ? (
+          <MediaCarousel
+            movies={currentUser?.topFiveMovies}
+            isMovie={true}
+            isTopFiveMovie={true}
+          />
+        ) : (
+          <p className="text-secondary p-5">
+            You haven't added any movies to your top five yet. Go to the Watched
+            page, add some movies, select your top five, and then come back
+            here.
+          </p>
+        )}
+
         <h2 className="text-secondary font-semibold text-xl">
           My Top 5 TV Shows
         </h2>
-        <MediaCarousel tvShows={currentUser?.topFiveTvShows} isMovie={false} />
+        {currentUser.topFiveTvShows.length > 0 ? (
+          <MediaCarousel
+            tvShows={currentUser?.topFiveTvShows}
+            isMovie={false}
+          />
+        ) : (
+          <p className="text-secondary p-5">
+            You haven't added any TV shows to your top five yet. Go to the
+            Watched page, add some TV shows, select your top five, and then come
+            back here.
+          </p>
+        )}
         <h2 className="text-secondary font-semibold text-xl">
           My Watched Movies
         </h2>
-        <MediaCarousel
-          movies={currentUser?.watchedMovies.toReversed().slice(0, 10)}
-          isMovie={true}
-        />
+        {currentUser.watchedMovies.length > 0 ? (
+          <MediaCarousel
+            movies={currentUser?.watchedMovies.toReversed().slice(0, 10)}
+            isMovie={true}
+          />
+        ) : (
+          <p className="text-secondary p-5">
+            You haven't added any movies to your watched list yet. Either go to
+            the Watched page, add some Movies, and then come back here, or click
+            on the TV icon on the movies below.
+          </p>
+        )}
         <h2 className="text-secondary font-semibold text-xl">
           My Watched TV Shows
         </h2>
-        <MediaCarousel
-          tvShows={currentUser?.watchedTvShows.toReversed().slice(0, 10)}
-          isMovie={false}
-        />
+        {currentUser.watchedTvShows.length > 0 ? (
+          <MediaCarousel
+            tvShows={currentUser?.watchedTvShows.toReversed().slice(0, 10)}
+            isMovie={false}
+          />
+        ) : (
+          <p className="text-secondary p-5">
+            You haven't added any TV shows to your watched list yet. Either go
+            to the Watched page, add some TV shows, and then come back here, or
+            click on the TV icon on the TV shows below.
+          </p>
+        )}
         <h2 className="text-secondary font-semibold text-xl">
           Movies I Want To Watch
         </h2>
-        <MediaCarousel
-          movies={currentUser?.toWatchMovies.toReversed().slice(0, 10)}
-          isMovie={true}
-        />
+        {currentUser.toWatchMovies.length > 0 ? (
+          <MediaCarousel
+            movies={currentUser?.toWatchMovies.toReversed().slice(0, 10)}
+            isMovie={true}
+          />
+        ) : (
+          <p className="text-secondary p-5">
+            You haven't added any movies to your "To Watch" list yet. Either go
+            to the To Watch page, add some movies, and then come back here, or
+            click on the Star icon on the movies below.
+          </p>
+        )}
         <h2 className="text-secondary font-semibold text-xl">
           TV Shows I Want To Watch
         </h2>
-        <MediaCarousel
-          tvShows={currentUser?.toWatchTvShows.toReversed().slice(0, 10)}
-          isMovie={false}
-        />
+        {currentUser.toWatchTvShows.length > 0 ? (
+          <MediaCarousel
+            tvShows={currentUser?.toWatchTvShows.toReversed().slice(0, 10)}
+            isMovie={false}
+          />
+        ) : (
+          <p className="text-secondary p-5">
+            You haven't added any TV shows to your "To Watch" list yet. Either
+            go to the To Watch page, add some TV shows, and then come back here,
+            or click on the Star icon on the TV shows below.
+          </p>
+        )}
         <h2 className="text-secondary font-semibold text-xl">
           Trending Movies
         </h2>
